d8711d89a45809aea4b889bbd369ce99626edc2e
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-09 15:12:15 +0930

    subgraph redo

--- src/mappings/defifa-deployer.ts
+++ src/mappings/defifa-deployer.ts
@@ -1,65 +1,28 @@
-import { BigInt, DataSourceContext } from "@graphprotocol/graph-ts"
-import { LaunchGame as LaunchGameEvent } from "../../generated/DefifaDeployer/DefifaDeployer"
-import {
-  Contract as ContractEntity,
-  Governor,
-  Account
-} from "../../generated/schema"
-import {
-  DefifaNFT as Contract,
-  Governor as GovernorInstance
-} from "../../generated/templates"
-
-import { DefifaNFT } from "../../generated/templates/DefifaNFT/DefifaNFT"
-import { Governor as GovernorContract } from "../../generated/templates/Governor/Governor"
+import { BigInt, DataSourceContext } from "@graphprotocol/graph-ts";
+import { LaunchGame as LaunchGameEvent } from "../../generated/DefifaDeployer/DefifaDeployer";
+import { Contract as ContractEntity } from "../../generated/schema";
+import { DefifaNFT as Contract } from "../../generated/templates";
 
 export function handleLaunchGame(event: LaunchGameEvent): void {
-  let caller = event.params.caller
-  let delegate = event.params.delegate
-  let gameId = event.params.gameId
-  let governor = event.params.governor
-  let tokenUriResolver = event.params.tokenUriResolver
+  let caller = event.params.caller;
+  let delegate = event.params.delegate;
+  let gameId = event.params.gameId;
+  let tokenUriResolver = event.params.tokenUriResolver;
 
-  let contract = new ContractEntity(delegate)
-  let governorContract = new Governor(governor)
-  let governorAccount = new Account(governor)
+  let contract = new ContractEntity(delegate);
 
-  // let instance = DefifaNFT.bind(event.params.delegate)
+  contract.address = delegate;
+  contract.gameId = gameId;
+  contract.creator = caller;
+  contract.tokenUriResolver = tokenUriResolver;
+  contract.totalSupply = BigInt.fromI32(0);
 
-  contract.address = delegate
-  contract.gameId = gameId
-  contract.governor = governor
-  contract.creator = caller
-  contract.tokenUriResolver = tokenUriResolver
-  contract.totalSupply = BigInt.fromI32(0)
+  contract.save();
 
-  // let name = instance.try_name()
-  // if (!name.reverted) {
-  //   contract.name = name.value
-  // }
-  // let symbol = instance.try_symbol()
-  // if (!symbol.reverted) {
-  //   contract.symbol = symbol.value
-  // }
+  let defifaContractContext = new DataSourceContext();
+  defifaContractContext.setBigInt("gameId", gameId);
+  Contract.createWithContext(delegate, defifaContractContext);
 
-  contract.save()
-
-  // const COUNTING_MODE = GovernorContract.bind(governor).try_COUNTING_MODE()
-  governorContract.asAccount = governor
-  governorContract.defifaContract = delegate
-  // if (!COUNTING_MODE.reverted) {
-  //   governorContract.mode = COUNTING_MODE.value
-  // }
-  governorContract.save()
-
-  governorAccount.asGovernor = governor
-  governorAccount.save()
-
-  let defifaContractContext = new DataSourceContext()
-  defifaContractContext.setBigInt("gameId", gameId)
-  Contract.createWithContext(delegate, defifaContractContext)
-
-  let governorContext = new DataSourceContext()
-  governorContext.setBigInt("gameId", gameId)
-  GovernorInstance.createWithContext(governor, governorContext)
+  let governorContext = new DataSourceContext();
+  governorContext.setBigInt("gameId", gameId);
 }
